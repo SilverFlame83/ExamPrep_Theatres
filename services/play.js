67353401cbc24a433405614f97652fac095ac245
SilverFlame83 <imren_83@abv.bg>
2021-06-25 22:56:59 +0300

    Validation about existing plays added

--- services/play.js
+++ services/play.js
@@ -1,7 +1,7 @@
 const Play = require('../models/Play');
 
 async function getAllPlays(){
-   return Play.find({}).lean();
+   return Play.find({ public: true}).sort({createAt: -1}).lean();
 }
 
 async function getPlayById(id){
@@ -9,6 +9,12 @@ async function getPlayById(id){
 }
 
 async function createPlay(playData){
+    const pattern = newRegExp(`^${playData.title}$`, 'i');
+    const existing = await Play.find({title: {$regex:pattern}});
+
+    if(existing){
+        throw new Error('A play with this name already exist!')
+    }
     const play = new Play(playData);
 
     await play.save();
